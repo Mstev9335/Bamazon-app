ad223881667242c1474d89ccdfa707d15b3b1ac1
unknown <mstevens9335@yahoo.com>
2019-04-22 18:01:45 -0400

    files added

---
+++ bamazonCustomer.js
@@ -0,0 +1,51 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "root",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err
+    console.log("connected");
+    afterConnection();
+});
+
+
+function afterConnection() {
+    connection.query("SELECT * FROM products", function(err, results) {
+      if (err) throw err;
+      inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "rawlist",
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].item_name);
+            }
+            return choiceArray;
+          },
+          message: "What auction would you like to place a bid in?"
+        },
+        {
+          name: "bid",
+          type: "input",
+          message: "How much would you like to bid?"
+        }
+      ])
+ 
+      connection.end();
+    });
+  }
